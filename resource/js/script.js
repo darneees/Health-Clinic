2e8222c296b09cc6cc90fa807e0c554a70e70bde
Doug Sá <79709843+darneees@users.noreply.github.com>
2023-11-07 17:40:53 -0300

    Update script.js

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,39 +1,38 @@
 // scroll reveal
 
 ScrollReveal().reveal('.title', {
-    delay: 400,
-    distance: '50px',
-    origin: 'left',
-    duration: 700,
-    reset: true
+  delay: 400,
+  distance: '50px',
+  origin: 'left',
+  duration: 700,
+  reset: true
 });
 
 ScrollReveal().reveal('.person', {
-    delay: 500,
-    distance: '50px',
-    origin: 'left',
-    duration: 700,
-    reset: true
+  delay: 500,
+  distance: '50px',
+  origin: 'left',
+  duration: 700,
+  reset: true
 });
 
 ScrollReveal().reveal('.person_adress', {
-    delay: 600,
-    distance: '50px',
-    origin: 'left',
-    duration: 700,
-    reset: true
+  delay: 600,
+  distance: '50px',
+  origin: 'left',
+  duration: 700,
+  reset: true
 });
-
 
 // adress modal
 
 const section = document.querySelector('section'),
-    overlay = document.querySelector('.adress_overlay'),
-    showBtn = document.querySelectorAll('.adress_btn'),
-    closeBtn = document.querySelector('.close_btn');
+  overlay = document.querySelector('.adress_overlay'),
+  showBtn = document.querySelectorAll('.adress_btn'),
+  closeBtn = document.querySelector('.close_btn');
 
 showBtn.forEach(showBtn => {
-    showBtn.addEventListener("click", () => section.classList.add("active"));
+  showBtn.addEventListener("click", () => section.classList.add("active"));
 });
 
 overlay.addEventListener("click", () => section.classList.remove("active"));
@@ -44,17 +43,17 @@ closeBtn.addEventListener("click", () => section.classList.remove("active"));
 // clock
 
 function atualizarRelogio() {
-    const relogio = document.getElementById('clock');
-    const data = new Date();
-    const hora = data.getHours();
-    const minutos = data.getMinutes();
-    const segundos = data.getSeconds();
+  const relogio = document.getElementById('clock');
+  const data = new Date();
+  const hora = data.getHours();
+  const minutos = data.getMinutes();
+  const segundos = data.getSeconds();
 
-    const horaFormatada = hora.toString().padStart(2, '0');
-    const minutosFormatados = minutos.toString().padStart(2, '0');
-    const segundosFormatados = segundos.toString().padStart(2, '0');
+  const horaFormatada = hora.toString().padStart(2, '0');
+  const minutosFormatados = minutos.toString().padStart(2, '0');
+  const segundosFormatados = segundos.toString().padStart(2, '0');
 
-    relogio.textContent = `${horaFormatada}:${minutosFormatados}:${segundosFormatados}`;
+  relogio.textContent = `${horaFormatada}:${minutosFormatados}:${segundosFormatados}`;
 }
 
 setInterval(atualizarRelogio, 1000);
@@ -63,7 +62,7 @@ setInterval(atualizarRelogio, 1000);
 // modal permission
 
 const btnVideo = document.querySelectorAll('.btn');
-const popup = document.getElementById('popupVideo');
+const popup = document.getElementById('popup');
 const overlayVideo = document.getElementById('overlayVideo');
 const permitirCamera = document.getElementById('allowCamera');
 const negarCamera = document.getElementById('denyCamera');
@@ -84,3 +83,17 @@ negarCamera.addEventListener('click', () => {
   popup.style.display = 'none';
   overlayVideo.style.display = 'none';
 });
+
+
+// load pages
+
+function loadPage(pageName) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main_container").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "resource/pages/" + pageName, true);
+  xhttp.send();
+}
